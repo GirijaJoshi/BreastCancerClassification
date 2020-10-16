1faee96b1e0fe86c25bbce8ccae26019acfe3420
tienl <tien.lee@gmail.com>
2020-10-16 01:12:51 -0700

    WIP - still need to update WC

--- static/js/index.js
+++ static/js/index.js
@@ -5,39 +5,70 @@ var myConfig = {
   options: {
     words: [
       {
-        text: "Age"
+        text: "Age",
+        count: 13
       },
       {
-        text: "Race"
+        text: "Race",
+          count: 1
       },
       {
-        text: "Family History"
+        text: "Family History",
+          count: 1
       },
       {
-        text: "First Menarche Age"
+        text: "First Menarche Age",
+          count: 1
       },
       {
-        text: "Age First Birth"
+        text: "Age First Birth",
+          count: 3
       },
       {
-        text: "BI-RAD Density"
+        text: "BI-RAD Density",
+          count: 6
       },
       {
-        text: "Hormone Therapy"
+        text: "Hormone Therapy",
+          count: 5
       },
       {
-        text: "Menopause"
+        text: "Menopause",
+          count: 1
       },
       {
         text: "BMI"
       },
       {
-        text: "Breast Biopsy"
+        text: "Breast Biopsy",
+          count: 1
       },
       {
-        text: "Breast Cancer History"
+        text: "Breast Cancer History",
+          count: 1
       }
-    ]
+    ],
+
+    style: {
+      borderRadius: 2,
+      fontFamily: 'Scope One',
+      padding: '5px 10px',
+      hoverState: {
+        alpha: 1,
+        backgroundColor: 'white',
+        borderColor: 0,
+        fontColor: 'black',
+        textAlpha: 1,
+      },
+      tooltip: {
+        rules: [{ //Rule 1
+          rule: '"%t" == "Age"',
+          text: "fdsfdsfds",
+          "font-weight": "bold",
+          "background-color": "#ff9999"
+        }]
+      }
+    }
   }
 };
 
