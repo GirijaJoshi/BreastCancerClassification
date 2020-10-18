d2612a9f17a74a976b8579b66394d43812620735
Girija Joshi <girija_aj@yahoo.com>
2020-10-18 16:10:21 -0700

    Tien HTML code

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+
+
+// we can delete this file if we want to use flask instead of javascript
+
+
+// Attach an event to listen for IMAGE CLICKS
+// calls PYTHON CODE WITH SPECIFIC IMAGES
+d3.selectAll("#ai1").on("click", RUN PREDICTOR PYTHON CODE WITH IMAGE 1);
+d3.selectAll("#ai2").on("click", RUN PREDICTOR PYTHON CODE WITH IMAGE 2);
+d3.selectAll("#ai3").on("click", RUN PREDICTOR PYTHON CODE WITH IMAGE 3);
+d3.selectAll("#ai4").on("click", RUN PREDICTOR PYTHON CODE WITH IMAGE 4);
+d3.selectAll("#ai5").on("click", RUN PREDICTOR PYTHON CODE WITH IMAGE 5);
